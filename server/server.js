eee9a8c8b5636058020d1b77b68a792f8a5b26a2
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2021-05-13 12:06:56 +0700

    Ready for Deployment

--- server/server.js
+++ server/server.js
@@ -2,16 +2,32 @@ import express from "express";
 import connectDB from "./config/db.js";
 import PostRoute from "./Routes/PostRoute.js";
 import CommentRoute from './Routes/CommentRoute.js'
+import path from 'path'
 
 const app = express();
 connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running....");
+// });
 app.use("/api/posts", PostRoute);
 app.use("/api/comment", CommentRoute);
+
+
+const __dirname = path.resolve()
+
+const MODE ='production';
+if (MODE === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.listen(5000, console.log("app is running on Port 5000"));
