c8df4fc246a5e9b80c84c39ba291526afacbd425
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2021-05-13 12:24:25 +0700

    make it better

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import PostRoute from "./Routes/PostRoute.js";
-import CommentRoute from "./Routes/CommentRoute.js";
-import path from "path";
+import CommentRoute from './Routes/CommentRoute.js'
+import path from 'path'
 
 const app = express();
 connectDB();
@@ -15,14 +15,19 @@ app.use(express.json());
 app.use("/api/posts", PostRoute);
 app.use("/api/comment", CommentRoute);
 
-const __dirname = path.resolve();
 
-const MODE = "production";
-if (MODE === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
+const __dirname = path.resolve()
+
+const MODE ='production';
+if (MODE === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
 app.listen(5000, console.log("app is running on Port 5000"));
